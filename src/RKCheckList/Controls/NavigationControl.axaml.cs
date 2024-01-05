42349b94cdb00c3b1b314cd856a5096da596962d
Roland König <roland.koenig@rolandk.de>
2024-01-05 22:18:12 +0100

    Some cleanup

--- src/RKCheckList/Controls/NavigationControl.axaml.cs
+++ src/RKCheckList/Controls/NavigationControl.axaml.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Avalonia;
 using Microsoft.Extensions.DependencyInjection;
 using RolandK.AvaloniaExtensions.DependencyInjection;
